6f3b846ab56dca6e019191f6bebf19e76cac3df0
Fabiana Correia <86627076+devfabiii@users.noreply.github.com>
2021-12-10 23:21:31 -0300

    Add files via upload

---
+++ quadrado-de-pares.cs
@@ -0,0 +1,23 @@
+using System;
+
+class DIO {
+
+    static void Main(string[] args) { 
+
+      int n = int.Parse(Console.ReadLine());
+  
+      if (n > 5 && n < 2000)
+      {
+  
+          for (var i = 1; i <= n; i++)
+          {
+              if (i % 2 == 0)
+              {
+                  Console.WriteLine(i + "^2 = " + i * i);
+              }
+          }
+      }
+
+    }
+
+}
